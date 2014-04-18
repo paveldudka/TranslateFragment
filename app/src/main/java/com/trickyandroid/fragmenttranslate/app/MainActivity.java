1abe07660cca714e6d7039f871dbbb7a9331daf8
Pavel Dudka <dudka.pavel@gmail.com>
2014-04-17 22:46:15 -0700

    initial commit

---
+++ app/src/main/java/com/trickyandroid/fragmenttranslate/app/MainActivity.java
@@ -0,0 +1,54 @@
+package com.trickyandroid.fragmenttranslate.app;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    private static final String LIST_FRAGMENT_TAG = "list_fragment";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_show_list) {
+            toggleList();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toggleList() {
+        Fragment f = getFragmentManager().findFragmentByTag(LIST_FRAGMENT_TAG);
+        if (f != null) {
+            getFragmentManager().popBackStack();
+        } else {
+            getFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.animator.slide_up,
+                            R.animator.slide_down,
+                            R.animator.slide_up,
+                            R.animator.slide_down)
+                    .add(R.id.list_fragment_container, SlidingListFragment
+                                    .instantiate(this, SlidingListFragment.class.getName()),
+                            LIST_FRAGMENT_TAG
+                    ).addToBackStack(null).commit();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/trickyandroid/fragmenttranslate/app/SlidingListFragment.java
@@ -0,0 +1,57 @@
+package com.trickyandroid.fragmenttranslate.app;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by paveld on 4/17/14.
+ */
+public class SlidingListFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.sliding_fragment_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setListAdapter(new MyListAdapter());
+    }
+
+    private class MyListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 30;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView result = (TextView) convertView;
+            if (result == null) {
+                result = (TextView) LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_view_item, parent, false);
+            }
+            result.setText("My custom element #" + position);
+
+            return result;
+        }
+    }
+}
